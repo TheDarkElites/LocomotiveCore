ef937b568a7b6466890460787ee6d485e5a49dd8
TheDarkElites <thedarkredstone@gmail.com>
2026-03-04 03:33:01 -0500

    initial commit, tracks, and mod setup

--- LocomotiveCore/LocomotiveCoreModSystem.cs
+++ LocomotiveCore/LocomotiveCoreModSystem.cs
@@ -3,14 +3,14 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-namespace ModTemplate
+namespace LocomotiveCore
 {
-    public class ModTemplateModSystem : ModSystem
+    public class LocomotiveCoreModSystem : ModSystem
     {
-        // Called on server and client
+        // Called on server and client 
         public override void Start(ICoreAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod: " + Lang.Get("mymodid:hello"));
+            Mod.Logger.Notification("Hello from template mod: " + Lang.Get("locomotivecore:hello"));
         }
 
         public override void StartServerSide(ICoreServerAPI api)
